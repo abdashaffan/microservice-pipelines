3ba8d2793294d73aaac42ed6ef54f4e46761d0d6
Abda Shaffan Diva <abdashaffan@gmail.com>
2019-07-02 10:17:03 +0700

    add vars

---
+++ vars/jenkinsJobs.groovy
@@ -0,0 +1,30 @@
+def call() {
+
+
+    node {
+        stage('Checkout') {
+            checkout scm
+        }
+        if (env.JOB_NAME.contains('deploy')) {
+            packageArtifact()
+        } else if (env.JOB_NAME.contains('test')) {
+            buildAndTest()
+        }
+    }
+}
+
+
+
+
+def packageArtifact() {
+    stage("Package artifact") {
+        sh 'mvn package'
+    }
+}
+
+def buildAndTest() {
+    stage("Backend tests") {
+        sh 'mvn test'
+    }
+}
+
